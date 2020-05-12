8c7dfd55abfd2fb42a2d65f75e137b6febb1277b
Hannah <hannah.gelblat@epfl.ch>
2020-05-12 09:40:49 +0200

    hannah cont.

--- code/Hannah.py
+++ code/Hannah.py
@@ -78,13 +78,47 @@ def Unet_method(img_dim):
     kernel_initializer='he_normal', padding='same')(c5)
 
     ##decoding path:
+    #   now dropout decreases again
     u6 = tf.keras.layers.Conv2DTranspose(128, (2,2), \
         strides=(2,2),padding='same')(c5)
     u6 = tf.keras.layers.concatenate([u6, c4])
+    c6 = tf.keras.layers.Conv2D(128, (3,3), activation='relu', \
+        kernel_initializer='he_normal', padding='same')(u6)
+    c6 = tf.keras.layers.Dropout(0.2)(c6)
+    c6 = tf.keras.layers.Conv2D(128, (3,3), activation='relu', \
+        kernel_initializer='he_normal', padding='same')(c6)
 
+    u7 = tf.keras.layers.Conv2DTranspose(64, (2,2), \
+        strides=(2,2),padding='same')(c6)
+    u7 = tf.keras.layers.concatenate([u7, c3])
+    c7 = tf.keras.layers.Conv2D(64, (3,3), activation='relu', \
+        kernel_initializer='he_normal', padding='same')(u7)
+    c7 = tf.keras.layers.Dropout(0.2)(c7)
+    c7 = tf.keras.layers.Conv2D(64, (3,3), activation='relu', \
+        kernel_initializer='he_normal', padding='same')(c7)
 
+    u8 = tf.keras.layers.Conv2DTranspose(16, (2,2), \
+        strides=(2,2),padding='same')(c7)
+    u8 = tf.keras.layers.concatenate([u8, c2])
+    c8 = tf.keras.layers.Conv2D(16, (3,3), activation='relu', \
+        kernel_initializer='he_normal', padding='same')(u8)
+    c8 = tf.keras.layers.Dropout(0.1)(c8)
+    c8 = tf.keras.layers.Conv2D(16, (3,3), activation='relu', \
+        kernel_initializer='he_normal', padding='same')(c8)
 
+    u9 = tf.keras.layers.Conv2DTranspose(16, (2,2), \
+        strides=(2,2),padding='same')(c8)
+    u9 = tf.keras.layers.concatenate([u9, c1])
+    c9 = tf.keras.layers.Conv2D(16, (3,3), activation='relu', \
+        kernel_initializer='he_normal', padding='same')(u9)
+    c9 = tf.keras.layers.Dropout(0.1)(c9)
+    c9 = tf.keras.layers.Conv2D(16, (3,3), activation='relu', \
+        kernel_initializer='he_normal', padding='same')(c9)
 
+    outputs = tf.keras.layers.Conv2D(1, (1,1), activation ='sigmoid')(c9)
+
+    model = tf.keras.Model(inputs=[inputs], outpus=[outputs])
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    #on peut tester avec adam et avec stochastic grad. descent
     
-
-    print(inputs)
+    model.summary()
