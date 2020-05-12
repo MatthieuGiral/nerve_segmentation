322c9650d730d5976cfb26ea60e035ae7daaafeb
Hannah <hannah.gelblat@epfl.ch>
2020-05-12 10:52:19 +0200

    cont. hannah

--- code/Hannah.py
+++ code/Hannah.py
@@ -3,29 +3,73 @@ import tensorflow as tf
 import os
 import matplotlib.image as mpimg
 from PIL import Image
+from tqdm import tqdm
 
 if __name__ == "__main__":
 
     print("test")
     #Paramètres: 
     n_train = 10 # nb images to load from train file
-    """
+    
     # Import images
 
     cd = os.getcwd()
-    file_test = os.path.join(cd, "..\data\test")
-    train_test = os.path.join(cd, "..\data\train")
+    #file_test = os.path.join(cd, "..\data\test\")
+    #train_test = os.path.join(cd, "..\data\train\")
 
-    #path_test = os.path.join(file_test, "*")
-    #path_train = os.path.join(file_train,"*")
+    path_train = '..\data\test\'
+    path_test = '..\data\test\'
+
+    train_ids = next(os.walk.(path_train))[1] #second item ?
+    test_ids = next(os.walk(path_test))[1]
+
+    #creating empty array, same sim as input image:
+    X_train = np.zeros((len(train_ids), img_height, img_width, img_channels), dtype=np.uint8) #dernier arg: unsigned integer
+    Y_train = np.zeros((len(train_ids), img_height, img_width, img_channels), dtype=np.bool) #what we try to predict. De type booléen (oui ou non pour chaque pixel)
+
+    #resizing of images:
+    for n, id_ in tqdm(enumerate(train_ids), total=len(train_ids)):
+        #accéder aux données ...
+        i = int(0) #counting pictures (only one over two is a real picture - the other is a mask)
+        path = path_train + id_
+        img = imread (path+ id_ + '.tif')
+        img = resize(img, (img_height, img_width), \
+            mode='constant', preserve_range=True)
+        if ((n%2 == 0) or (n == 0)):
+            X_train[i] = img
+        elif:
+            Y_train[i] = img
+            i += 1
+
+        """
+        mask = np.zeros((img_height, img_width, 1), dtype=np.bool)
+        for mask_file in next(os.walk(path + '\a\'))[2]:
+            mask_ = imread(path...)
+            mask_ = np.expand_dims(resize(mask_, (img_height, img_width), \
+                mode='constant', preserve_range=True), axis =1)
+            mask = np.maximum(mask, mask_)
+
+        Y_train[n] = mask
+        """
+
+
+
+
+
+
+
+
+    path_train = os.path.join(file_train,"*")
+
+    path_test = os.path.join(file_test, "*")
 
     data_train = []
     for i in range (n_train):
         data_train.append(Image.open(path_test)) #liste d'images
     # définir l'input -> mettre sous forme d'une matrice
     # quelles dimensions ?
-    """
-    img_dim = [1, 2, 3]
+    
+    img_dim = [img_height, img_width, img_channels]
 
     Unet_method(X, Y, img_dim)
 
