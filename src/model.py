c4a89a71ffabbd83b31c3500516213b7caf416ee
Matthieu Giral <matthieu.giral@hotmail.com>
2020-05-15 17:45:38 +0200

    factorisation of Matthieu and Hannah

--- src/model.py
+++ src/model.py
@@ -94,12 +94,6 @@ class segmenter():
         return model
 
     def train(self, X,Y, epochs, batch_size):
-        # checkpointer = tf.keras.callbacks.ModelCheckpoint('model_file', \
-        # callbacks = [
-        # tf.keras.callbacks.EarlyStopping(patience =2, monitor='valid_loss'), #restore_best_weights=True ? #stoppe le training quand valid_loss est minimisée
-        # tf.keras.callbacks.TensorBoard(log_dir='logs', update_freq = 'epoch')] #store l'évolution des test metrics a chaque epochs e
-
-
         results = self.model.fit(X, Y, validation_split=0.1, batch_size=batch_size, epochs=epochs) #, callbacks=callbacks
         training_curves(results)
         self.is_trained=True
